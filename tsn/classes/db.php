b3f10b4a92f39a405b7fed0f537862d4124e79a7
VitaliyIlinov <ilinov123@ukr.net>
2017-02-14 00:04:19 +0200

    pdo connect

--- tsn/classes/db.php
+++ tsn/classes/db.php
@@ -1,19 +1,22 @@
 <?php
 
-namespace my\db;
 class Db
 {
-
-    static function getConnection()
-    {
+    protected $connect=null;
+    public function __construct() {
         // Получаем параметры подключения из файла
-        $paramsPath = ROOT . '/config/db_params.php';
-        $params = include($paramsPath);
+        $params = Config::get('db_connection');
         // Устанавливаем соединение
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
-        $db = new PDO($dsn, $params['user'], $params['password']);
-        // Задаем кодировку
-        $db->exec("set names utf8");
-        return $db;
+        $dsn = "mysql:host={$params['host']};dbname={$params['db_name']};charset=utf8";
+        $opt = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        );
+        $this->connect = new PDO($dsn, $params['user'], $params['password'], $opt);
+    }
+
+    public function getUsers()
+    {
+        return $this->connect->query('SELECT * FROM users');
     }
 }
--- tsn/config/db_params.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Массив с параметрами подключения к базе данных
-return array(
-    'host' => 'localhost',
-    'dbname' => 'test',
-    'user' => 'root',
-    'password' => '',
-);
