bd0285593bd14bc9a5a3c2b08e61b63992e847a8
vitaliyIlinov <ilinov123@ukr.net>
2017-02-13 18:58:52 +0200

    PHP conntect to Mysql PDO_v1

--- tsn/classes/db.php
+++ tsn/classes/db.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace my\db;
-class Db {
+class Db
+{
 
-    static function getConnection(){
-        $db = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
-    }
-    private function getConfig (){
+    static function getConnection()
+    {
+        // Получаем параметры подключения из файла
         $paramsPath = ROOT . '/config/db_params.php';
+        $params = include($paramsPath);
+        // Устанавливаем соединение
+        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+        $db = new PDO($dsn, $params['user'], $params['password']);
+        // Задаем кодировку
+        $db->exec("set names utf8");
+        return $db;
     }
 }
