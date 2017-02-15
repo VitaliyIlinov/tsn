fceec9000a48f131a515050b418a42b6eefa6b32
vitaliyIlinov <ilinov123@ukr.net>
2017-02-15 18:50:04 +0200

    with sql structure

---
+++ structure.sql
@@ -0,0 +1,47 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               10.1.16-MariaDB - mariadb.org binary distribution
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Дамп структуры для таблица lemon_tsn.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `second_name` varchar(50) NOT NULL,
+  `citizen` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `link_fb` varchar(100) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `foreign_passport` set('yes','no') DEFAULT NULL,
+  `shengen_visa` set('yes','no') DEFAULT NULL,
+  `visa_own` set('yes','no') DEFAULT NULL,
+  `lodging_with_pay` set('yes','no') DEFAULT NULL,
+  `lodging_without_pay` set('yes','no') DEFAULT NULL,
+  `years_sup_football` tinyint(4) DEFAULT NULL,
+  `fb_support` text,
+  `fb_in_your_life` text,
+  `sup_in_foreign_country` set('yes','no') DEFAULT NULL,
+  `matches` text,
+  `create_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `edit_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `activate` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Экспортируемые данные не выделены.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
